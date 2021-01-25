6940ca6cad71c35decce972e84594d999420faba
kajos12340 <piotr.kajka.96@gmail.com>
2021-01-25 16:15:44 +0100

    room added

--- src/controllers/ShoppingListController/ShoppingListController.ts
+++ src/controllers/ShoppingListController/ShoppingListController.ts
@@ -14,30 +14,15 @@ class AuthController implements ISocketController {
   }
 
   public initSocketActions() {
-    console.log('initSocketActions');
-    const getApiAndEmit = socket => {
-      const response = new Date();
-      // Emitting a new message. Will be consumed by the client
-      socket.emit("FromAPI", response);
-    };
-
-    let interval;
-
     this.socketServer.on("connect", (socket) => {
       socket.on('joinRoom', (user) => {
         this.clients.push(user.name);
 
-        this.socketServer.emit('addedUser', { message: `user: ${user.name} has joined the room!!!`, currentUsers: this.clients });
+        socket.join(user.roomName);
+
+        this.socketServer.in(user.roomName).emit('addedUser', { message: `user: ${user.name} has joined the room!!!`, currentUsers: this.clients });
       });
       console.log("New client connected");
-      if (interval) {
-        clearInterval(interval);
-      }
-      interval = setInterval(() => getApiAndEmit(socket), 1000);
-      socket.on("disconnect", () => {
-        console.log("Client disconnected");
-        clearInterval(interval);
-      });
     });
   }
 
