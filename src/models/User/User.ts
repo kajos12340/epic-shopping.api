a55af89b2246bd2a4fe527dad10f26382a2d534a
kajos12340 <piotr.kajka.96@gmail.com>
2021-01-27 20:46:24 +0100

    user model update pt2

--- src/models/User/User.ts
+++ src/models/User/User.ts
@@ -7,6 +7,7 @@ export interface IUser {
   email: string,
   registrationDate: Date,
   lastLoginDate?: Date,
+  isConfirmed?: Date,
 }
 
 interface IUserDocument extends IUser, Document { }
@@ -40,6 +41,9 @@ const userSchema = new Schema<IUserDocument, IUserModel>({
   },
   lastLoginDate: {
     type: Date,
+  },
+  isConfirmed: {
+    type: Boolean,
   },
 });
 
